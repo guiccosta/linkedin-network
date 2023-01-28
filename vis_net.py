18f48f39b03fcc94089dd074c9800eb5a054d574
Guilherme Corsini Costa <guiccosta>
2023-01-28 11:56:41 -0300

    Fazendo algumas mudancas de acordo a PEP8

--- vis_net.py
+++ vis_net.py
@@ -1,9 +1,7 @@
 import streamlit as st
-import streamlit.components.v1 as components
 import pandas as pd
-import networkx as nx
-from pyvis.network import Network
-import itertools
+from functions import create_network, visualize_network
+
 
 st.title('Visualizando sua rede de conexões do Linkedin')
 st.write("""
@@ -15,47 +13,14 @@ Para obter seus dados, siga o link: https://www.linkedin.com/help/linkedin/answe
 upload_file = st.file_uploader('Escolha um arquivo:')
 
 if upload_file is not None:
-    connections = pd.read_csv(upload_file)
-    buttom = st.button('Visualizar a rede - **_Aguarde a barra de progresso carregar_**')
+    connections = pd.read_csv(upload_file, skiprows=3)
+    button = st.button('Visualizar a rede - **_Aguarde a barra de progresso carregar_**')
     st.dataframe(connections)
-# connections = pd.read_csv('Connections.csv')
 
+    if button:
+        H = create_network(connections)
+        visualize_network(H)
 
-
-    if buttom:
-        H = nx.Graph(notebook=True)
-
-        for i in connections.index:
-            H.add_node(i+1)
-
-        nome = dict(connections['First Name'])
-        company = dict(connections['Company'])
-
-        nx.set_node_attributes(H, nome, "title")
-        nx.set_node_attributes(H, company, "group")
-
-        H.nodes[connections.index[-1]+1]["title"] = "Você"
-        H.nodes[connections.index[-1]+1]["group"] = "Onde voce trabalha"
-
-        for i in connections.index:
-            H.add_edge(i, connections.index[-1]+1)
-
-        companies = dict()
-
-        for company in set(connections["Company"]):
-            companies[company] = list(connections[connections["Company"] == company].index)
-
-        for company in companies.keys():
-            H.add_edges_from(list(itertools.combinations(companies[company], 2)))
-        low_deg = [node for node in H.nodes if len(list(nx.neighbors(H, node))) == 1]
-        H.remove_nodes_from(low_deg)
-        nt = Network('500px', '500px')
-        nt.from_nx(H)
-        nt.show('nt.html', local=True)
-        HtmlFile = open("nt.html", 'r', encoding='utf-8')
-        source_code = HtmlFile.read() 
-        components.html(source_code, height=500, width=500)
-        
 st.warning("""
 Caso você não se sinta confortável em fazer o upload dos seus dados, clique no botão abaixo para 
 utilizar dados aleatórios
@@ -64,35 +29,5 @@ buttom_random = st.button('Visualizar rede com dados aleatórios - **_Aguarda a 
 if buttom_random:
     random_file = 'Connections-random-names.csv'
     connections = pd.read_csv(random_file)
-    H = nx.Graph(notebook=True)
-
-    for i in connections.index:
-        H.add_node(i + 1)
-
-    nome = dict(connections['First Name'])
-    company = dict(connections['Company'])
-
-    nx.set_node_attributes(H, nome, "title")
-    nx.set_node_attributes(H, company, "group")
-
-    H.nodes[connections.index[-1] + 1]["title"] = "Você"
-    H.nodes[connections.index[-1] + 1]["group"] = "Onde voce trabalha"
-
-    for i in connections.index:
-        H.add_edge(i, connections.index[-1] + 1)
-
-    companies = dict()
-
-    for company in set(connections["Company"]):
-        companies[company] = list(connections[connections["Company"] == company].index)
-
-    for company in companies.keys():
-        H.add_edges_from(list(itertools.combinations(companies[company], 2)))
-    low_deg = [node for node in H.nodes if len(list(nx.neighbors(H, node))) == 1]
-    H.remove_nodes_from(low_deg)
-    nt = Network('500px', '500px')
-    nt.from_nx(H)
-    nt.show('nt.html', local=True)
-    HtmlFile = open("nt.html", 'r', encoding='utf-8')
-    source_code = HtmlFile.read() 
-    components.html(source_code, height=800, width=800)
+    H = create_network(connections)
+    visualize_network(H)
